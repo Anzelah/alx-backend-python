6fcbf80b81e0860ddc18ad27ca069143910d587f
Anzelah <jepkoechanzellah@gmail.com>
2024-01-25 12:39:07 +0000

    ue math.floor() function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Import modules"""
+
+import math
+"""Imports modules"""
+
+
+def floor(n: float) -> int:
+    """Convert our float into a floor"""
+    return math.floor(n)
