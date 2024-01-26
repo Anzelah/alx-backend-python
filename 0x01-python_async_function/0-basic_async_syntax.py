ec891b72c4083d95b64d85e5477f697262ab2380
Anzelah <jepkoechanzellah@gmail.com>
2024-01-26 18:06:11 +0000

    fix type hiting

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 """Import modules"""
 
 
-async def wait_random(max_delay=10: int) -> float:
+async def wait_random(max_delay: int=10) -> float:
     """Asynchronous function using random module"""
     await asyncio.sleep(0, max_delay)
     return random.uniform(0, max_delay)
