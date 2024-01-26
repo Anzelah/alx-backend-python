81aeb3df25d56ed85dab05b98a678b18bcd52fc8
Anzelah <jepkoechanzellah@gmail.com>
2024-01-26 18:00:22 +0000

    seing if it passes checker

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defining the functins"""
+
+import asyncio
+import random
+"""Import modules"""
+
+
+async def wait_random(max_delay=10):
+    """Asynchronous function using random module"""
+    await asyncio.sleep(0, max_delay)
+    return random.uniform(0, max_delay)
