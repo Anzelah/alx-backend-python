91ea2f79a860a26830e7dc66079ea54b9076bede
Anzelah <jepkoechanzellah@gmail.com>
2024-01-27 12:58:36 +0000

    add documentation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Use async cmrehension to return random numbers"""
     return [i async for i in async_generator()]
