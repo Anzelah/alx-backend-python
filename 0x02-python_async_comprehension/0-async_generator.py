75d752597a2786d39d05f6d1459e538e4259ce76
Anzelah <jepkoechanzellah@gmail.com>
2024-01-27 12:02:04 +0000

    a generator function always returns an iterable object

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """Create a coroutine"""
+
 import asyncio
 import random
+from typing import Iterator
 
 
-async def async_generator() -> float:
-    for i in range(10):
+async def async_generator() -> Iterator[float]:
+    """Yield a random number between 0 and 10"""
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
