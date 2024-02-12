0ff3cae7e5dad957916d694c0c5e61566c8ec10f
Anzelah <jepkoechanzellah@gmail.com>
2024-02-12 16:42:14 +0000

    test that an exception is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+    
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"])
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that a key error is raised for inputs"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == '__main__':
