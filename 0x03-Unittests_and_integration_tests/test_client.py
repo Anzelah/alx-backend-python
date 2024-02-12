c1d43cffe9bdd88456a8db0c4b31b4e6e645218e
Anzelah <jepkoechanzellah@gmail.com>
2024-02-12 21:42:12 +0000

    use porperty mock to mock the function properties' return value

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Import your modules"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -15,10 +15,21 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc")
         ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_getjson):
+    def test_org(self, name, mock_getjson):
         """Test that the org function returns the correct value
         """
-        instance = GithubOrgClient(org_name)
+        instance = GithubOrgClient(name)
         instance.org()
-        org_client = "https://api.github.com/orgs/{}".format(org_name)
+        org_client = "https://api.github.com/orgs/{}".format(name)
         mock_getjson.assert_called_once_with(org_client)
+
+    @parameterized.expand([
+        ("some_url", {"repos_url": "https://urls.com"})
+        ])
+    def test_public_repos_url(self, name, expected_result):
+        """Test the public repos url function of the githhuborgclient class
+        """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=expected_result)):
+            result = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(result, expected_result["repos_url"])
